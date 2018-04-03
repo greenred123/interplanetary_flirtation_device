5b91d850f1a2d84a388117fd00bb102c13f54b18
greenred123 <c.langford@mailbox.org>
2018-04-03 12:51:17 +0930

    add frontend files

---
+++ ifd/public/shaders/wiggle-frag.js
@@ -0,0 +1,13 @@
+precision highp float;
+precision highp int;
+
+uniform vec3 color;
+
+varying float vNoise;
+
+void main() {
+    
+    gl_FragColor = vec4( color * clamp( vNoise, 0.0, 0.1 ), 0.1 );
+
+}
+  
